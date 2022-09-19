caae02fedfccf8f7d423fffb738f5213e4d5cfe7
d21it178 <90238639+d21it178@users.noreply.github.com>
2022-09-19 12:45:44 +0530

    last

--- src/main/webapps/src/components/Register.js
+++ src/main/webapps/src/components/Register.js
@@ -23,9 +23,9 @@ export default function Register() {
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+      </Form.Group> */}
       <Button variant="primary" type="submit">
         Submit
       </Button>
